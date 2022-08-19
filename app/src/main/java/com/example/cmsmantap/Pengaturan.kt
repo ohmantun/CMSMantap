41f760d8df8e9fa48ac9d7ef4e9b1d4e9bf28d83
ohmantun <rakhapandji@gmail.com>
2022-08-20 01:13:04 +0700

    update pengaturan

--- app/src/main/java/com/example/cmsmantap/Pengaturan.kt
+++ app/src/main/java/com/example/cmsmantap/Pengaturan.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.cardview.widget.CardView
+import com.example.cmsmantap.data.LoginActivity
 
 class Pengaturan : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +17,15 @@ class Pengaturan : AppCompatActivity() {
       //  val btnPayrollUmum = findViewById<ImageButton>(R.id.btnPayroll_umum)
       //  val btnPayrollPensiunan = findViewById<ImageButton>(R.id.btnPayroll_pensiunan)
         val btnBack = findViewById<ImageView>(R.id.btnBack)
+        val btnGantiSandi = findViewById<CardView>(R.id.btnGantiSandi)
 
         btnBack.setOnClickListener{
             val intent = Intent(this, Beranda::class.java)
+            startActivity(intent)
+        }
+
+        btnGantiSandi.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java) //Login Activity ganti ke page sandinya
             startActivity(intent)
         }
 
