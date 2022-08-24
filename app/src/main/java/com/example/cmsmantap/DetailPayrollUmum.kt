26161c0b20dd1a072269aa042581f7fe899fbfee
erdna26 <dariustama26@gmail.com>
2022-08-24 23:38:41 +0700

    nambah page untuk checker

---
+++ app/src/main/java/com/example/cmsmantap/DetailPayrollUmum.kt
@@ -0,0 +1,11 @@
+package com.example.cmsmantap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailPayrollUmum : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_payroll_umum)
+    }
+}
