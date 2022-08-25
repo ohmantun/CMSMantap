e44a5b809a9e81f6178b514a0f43cee95e7e16b2
ohmantun <rakhapandji@gmail.com>
2022-08-25 21:58:19 +0700

    ganti sandi fix tanpa fungsi

---
+++ app/src/main/java/com/example/cmsmantap/GantiSandi.kt
@@ -0,0 +1,14 @@
+package com.example.cmsmantap
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class GantiSandi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_CMSMantap)
+        setContentView(R.layout.activity_ganti_sandi)
+
+    }
+}
